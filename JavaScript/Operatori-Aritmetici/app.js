19a84bf8e89b6580a2440ca6a9df1593c85c0f4e
Dragos Ionicescu <ionicescudragos2000@gmail.com>
2024-06-18 18:09:45 +0300

    Create app.js

---
+++ JavaScript/Operatori-Aritmetici/app.js
@@ -0,0 +1,39 @@
+//1
+
+let apples=10;
+let applesAway=3;
+let applesLeft= apples - applesAway;
+console.log(applesLeft);
+
+//2
+
+let score1= 85;
+let score2= 90;
+let score3= 78;
+let avgScore= (score1 + score2 + score3) / 3;
+console.log(avgScore);
+
+//3
+
+let book= 15;
+let totalCost= book * 8;
+console.log(totalCost);
+
+//4
+
+let remainder= 29%5;
+console.log(remainder);
+
+//5
+
+let speed= 60;
+let time= 3;
+let distance= speed * time;
+console.log(distance);
+
+//6
+
+let price= 10;
+let days= 7;
+let savings= price * days;
+console.log(savings);
