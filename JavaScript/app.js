b4f839166e6a31830acb0f08037f6089a2fddaa7
Dragos Ionicescu <ionicescudragos2000@gmail.com>
2024-06-20 14:41:57 +0300

    op-comp

---
+++ JavaScript/app.js
@@ -0,0 +1,15 @@
+//1
+
+let laur= 85%;
+let notaMin= 5;
+let notaLaur= (85 / 100) * 10;
+let admis= notaLaur >= notaMin;
+console.log(admis);
+
+//2
+
+let latime = 4;
+let metruCaramida = 1;
+let totalCaramizi = 17;
+let ultimNivelComplet = (totalCaramizi % latime) === 0;
+console.log(ultimNivelComplet);
