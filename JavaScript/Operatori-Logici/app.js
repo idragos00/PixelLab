a78e9422bbb25c43430ca306fe50433917977f38
Dragos Ionicescu <ionicescudragos2000@gmail.com>
2024-06-25 18:33:24 +0300

    op-logici

---
+++ JavaScript/Operatori-Logici/app.js
@@ -0,0 +1,44 @@
+//1
+
+console.log("Primul scenariu");
+let X = 3;
+
+let lunaAlegeri = 6;
+let putemVota = lunaAlegeri ==  X;
+console.log(putemVota);
+
+
+console.log("Al doilea scenariu");
+let X = 6;
+
+let lunaAlegeri = 6;
+let putemVota = lunaAlegeri ==  X;
+console.log(putemVota);
+
+//2
+
+console.log("Primul scenariu");
+let Y = 2010;
+let anCurent = 2024;
+let varstaVot = 18;
+
+let putemVota = (anCurent - Y) >= varstaVot;
+console.log(putemVota);
+
+
+console.log("Al doilea scenariu");
+let Y = 1990;
+let anCurent = 2024;
+let varstaVot = 18;
+
+let putemVota = (anCurent - Y) >= varstaVot;
+console.log(putemVota);
+
+//3
+
+
+
+
+
+
+
