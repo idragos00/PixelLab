f2cc0ee89a4b72ef28f0e53579324dd69bedabce
Dragos Ionicescu <ionicescudragos2000@gmail.com>
2024-06-27 18:58:22 +0300

    else-if

---
+++ JavaScript/Else-If/app.js
@@ -0,0 +1,16 @@
+
+let x= ;
+
+if(x%3==0 && x%5==0){
+    console.log("fizzbuzz")   
+}
+
+else if (x%3==0){
+    console.log("fizz")   
+}
+else if (x%5==0){
+    console.log("buzz")
+}
+else {
+    console.log(x)
+}
