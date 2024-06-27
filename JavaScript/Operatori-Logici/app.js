153dcb8c439a1d09e3ee811b50f4ac94461ab432
Dragos Ionicescu <ionicescudragos2000@gmail.com>
2024-06-27 19:11:05 +0300

    .

--- JavaScript/Operatori-Logici/app.js
+++ JavaScript/Operatori-Logici/app.js
@@ -36,6 +36,30 @@ console.log(putemVota);
 
 //3
 
+// Datele fixe ale problemei
+const varstaMinimaVot = 18;
+const anCurent = 2024;
+const lunaVotarii = 6;
+
+// Datele de scenariu
+let x = 3;
+let y = 1990;
+console.log("Scenariu:  x=" + x + " y=" + y);
+
+//Inceput Exercitiu 1
+
+const suntDeschiseUrnele = x === lunaVotarii;
+console.log("exercitiul1:" + suntDeschiseUrnele);
+
+// Inceput exercitiu 2
+const dataNastereWanda = y;
+const varstaWanda = anCurent - dataNastereWanda;
+const poateSaVoteze = varstaWanda >= varstaMinimaVot;
+console.log("exercitiul2:" + poateSaVoteze);
+
+//Inceput Exercitiu 3
+console.log("exercitiul3:" + (suntDeschiseUrnele && poateSaVoteze));
+
 
 
 
